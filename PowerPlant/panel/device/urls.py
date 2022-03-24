906e43a23a9216219b8873dbd5891791c14e3cb5
Fernando Cota Rodriguez <ing.fernandocota@gmail.com>
2022-03-23 20:43:14 -0700

    device url fix

--- PowerPlant/panel/device/urls.py
+++ PowerPlant/panel/device/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('device/', views.DeviceListViews.as_view(), name='device_list'),
+    path('devices/', views.DeviceListViews.as_view(), name='device_list'),
     path('device/<int:pk>/', views.DeviceDetailViews.as_view(), name="device_detail"),
 
     path('readings/', views.ReadingsViews.as_view(), name="readings"),
